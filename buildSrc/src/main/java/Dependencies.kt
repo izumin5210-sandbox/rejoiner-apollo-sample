cf2a5021890ae3c3a9f69437e225cddc14eea3e2
izumin5210 <m@izum.in>
2019-12-31 10:20:10 +0900

    Update dependencies for rejoiner

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,14 +1,23 @@
 object Dependencies {
     object Versions {
         const val KOTLIN = "1.3.60"
-        const val PROTOC = "3.11.2"
+        const val PROTOBUF = "3.11.2"
         const val GRPC_JAVA = "1.26.0"
         const val GOOGLE_APIS_COMMON_PROTOS = "0.0.3"
         const val JAVAX_ANNOTATION = "1.3.2"
+
+        const val GUAVA = "28.0-jre"
+        const val PROTO_GOOGLE_COMMON_PROTOS = "1.16.0"
+
+        const val REJOINER = "0.3.0"
+        const val JETTY = "9.3.8.v20160314"
+        const val SLF4J = "1.6.2"
+        const val GUICE = "4.2.0"
+        const val FUTURE_CONVERTER_JAVA8_GUAVA = "1.1.0"
     }
 
     const val KOTLIN_STD_LIB = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.KOTLIN}"
-    const val PROTOC = "com.google.protobuf:protoc:${Versions.PROTOC}"
+    const val PROTOC = "com.google.protobuf:protoc:${Versions.PROTOBUF}"
     const val GRPC_JAVA = "io.grpc:protoc-gen-grpc-java:${Versions.GRPC_JAVA}"
     const val GRPC_NETTY = "io.grpc:grpc-netty:${Versions.GRPC_JAVA}"
     const val GRPC_PROTOBUF = "io.grpc:grpc-protobuf:${Versions.GRPC_JAVA}"
@@ -16,4 +25,15 @@ object Dependencies {
     const val GOOGLE_APIS_COMMON_PROTOS =
         "com.google.api.grpc:googleapis-common-protos:${Versions.GOOGLE_APIS_COMMON_PROTOS}"
     const val JAVAX_ANNOTATION = "javax.annotation:javax.annotation-api:${Versions.JAVAX_ANNOTATION}"
+
+    const val PROTOBUF_JAVA_UTIL = "com.google.protobuf:protobuf-java-util:${Versions.PROTOBUF}"
+    const val GUAVA = "com.google.guava:guava:${Versions.GUAVA}"
+    const val PROTO_GOOGLE_COMMON_PROTOS = "com.google.api.grpc:proto-google-common-protos:${Versions.PROTO_GOOGLE_COMMON_PROTOS}"
+
+    const val REJOINER = "com.google.api.graphql:rejoiner:${Versions.REJOINER}"
+    const val JETTY = "org.eclipse.jetty:jetty-server:${Versions.JETTY}"
+    const val JETTY_SERVLET = "org.eclipse.jetty:jetty-servlet:${Versions.JETTY}"
+    const val SLF4J = "org.slf4j:slf4j-simple:${Versions.SLF4J}"
+    const val GUICE_SERVLET = "com.google.inject.extensions:guice-servlet:${Versions.GUICE}"
+    const val FUTURE_CONVERTER_JAVA8_GUAVA = "net.javacrumbs.future-converter:future-converter-java8-guava:${Versions.FUTURE_CONVERTER_JAVA8_GUAVA}"
 }
