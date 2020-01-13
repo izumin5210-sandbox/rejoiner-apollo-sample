159c57c41aba201baef8de62cd9846e320283f21
izumin5210 <m@izum.in>
2020-01-13 17:20:33 +0900

    Refactor

--- bff-rejoiner/src/main/kotlin/dev/izumin/sandbox/rejoiner/bff/loader/GitHubDataLoaderModule.kt
+++ bff-rejoiner/src/main/kotlin/dev/izumin/sandbox/rejoiner/bff/loader/GitHubDataLoaderModule.kt
@@ -1,41 +1,30 @@
 package dev.izumin.sandbox.rejoiner.bff.loader
 
-import com.google.common.base.Function
-import com.google.common.util.concurrent.Futures
-import com.google.common.util.concurrent.ListenableFuture
-import com.google.common.util.concurrent.MoreExecutors
 import com.google.inject.AbstractModule
 import com.google.inject.Provides
 import dev.izumin5210.sandbox.github.ListUsersRequest
 import dev.izumin5210.sandbox.github.User
 import dev.izumin5210.sandbox.github.UserServiceGrpc
 import net.javacrumbs.futureconverter.java8guava.FutureConverter
-import org.dataloader.BatchLoader
 import org.dataloader.DataLoader
 import org.dataloader.DataLoaderOptions
 import org.dataloader.DataLoaderRegistry
+import org.dataloader.MappedBatchLoader
 
 class GitHubDataLoaderModule : AbstractModule() {
     @Provides
     fun dataLoaderRegistry(userClient: UserServiceGrpc.UserServiceFutureStub): DataLoaderRegistry {
-        val userBatchLoader = BatchLoader<String, User> { keys ->
+        val userBatchLoader = MappedBatchLoader<String, User> { keys ->
             val req = ListUsersRequest.newBuilder()
                     .addAllLogins(keys.filterNot { it.isEmpty() })
                     .build()
-            val lf: ListenableFuture<List<User?>> =
-                    Futures.transform(
-                            userClient.listUsers(req),
-                            Function { resp ->
-                                val m = resp!!.usersList.map { it.login to it }.toMap()
-                                keys.map { m[it] }
-                            },
-                            MoreExecutors.directExecutor()
-                    );
-            FutureConverter.toCompletableFuture(lf)
+            FutureConverter.toCompletableFuture(userClient.listUsers(req))
+                    .thenApply { it!!.usersList.associateBy { it.login } }
         }
 
         val registry = DataLoaderRegistry()
-        registry.register("github/users", DataLoader(userBatchLoader, DataLoaderOptions.newOptions().setMaxBatchSize(5)))
+        registry.register("github/users", DataLoader.newMappedDataLoader(
+                userBatchLoader, DataLoaderOptions.newOptions().setMaxBatchSize(5)))
         return registry
     }
 }
